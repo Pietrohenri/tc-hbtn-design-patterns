3648b3568590f8a400fa2c73af5f218be414440a
CIANDT\pietros <pietros@ciandt.com>
2022-07-14 09:54:03 -0300

    salario

---
+++ 0x01/salario/CalculadorSalarioService.java
@@ -0,0 +1,26 @@
+package salario;
+import java.util.UUID;
+
+public class CalculadorSalarioService {
+    private static CalculadorSalarioService instancia;
+    public UUID uuid = UUID.randomUUID();
+
+    private CalculadorSalarioService(CalculadorSalarioService instancia) {
+        this.instancia = instancia;
+
+    }
+
+    public static CalculadorSalarioService getCalculadorSalarioService(){
+        if(instancia == null){
+            instancia = new CalculadorSalarioService(instancia);
+        }
+        return instancia;
+
+    }
+
+    public double calcularSalarioLiquido(double salarioBruto, double valorDescontos, double valorVendas, double percentualComissao){
+        return salarioBruto - valorDescontos + (valorVendas * (percentualComissao / 100));
+    }
+
+
+}
